eabec8702cf2e85dd01b1d2183e163b77a63bc68
Yoga Pratama Firmansyah <37876340+yogasyah@users.noreply.github.com>
2018-03-29 09:23:40 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,15 @@ public class Main {
         int n = 12/2;
         for(int i=1;i<=n;i++){
             for(int k=n;k>i;k--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int j=1;j<=l;j++){
                 System.out.print("+");
             }
+            for(int k=n;k>i;k--){
+                System.out.print("0");
+            }
+            
             l+=2;
 
             System.out.println("");
@@ -21,10 +25,13 @@ public class Main {
 
         for(int i=(n-1);i>=1;i--){
             for(int k=i;k<=(n-1);k++){
-                System.out.print(" ");
+                System.out.print("O");
             }
             for(int j=l;j>=1;j--){
                 System.out.print("+");
+            }
+            for(int k=i;k<=(n-1);k++){
+                System.out.print("O");
             }
             l-=2;
 
